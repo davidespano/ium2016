74971d555d3dcf16505ce0a7fd8faaf96d7e6f4b
AlessioMurru <murru.ale91@gmail.com>
2016-11-07 10:52:26 +0100

    no message

---
+++ Esercitazione_3/app/src/main/java/com/example/tutor_ium/esercitazione_3/ControlTest.java
@@ -0,0 +1,13 @@
+package com.example.tutor_ium.esercitazione_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_test);
+    }
+}
